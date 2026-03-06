b491d530c3407f673cd848d893a8641b84b4f370
dorra sellami <dorra-sellami@hotmail.fr>
2026-03-06 15:13:37 +0100

    My first website

--- app.py
+++ app.py
@@ -5,38 +5,33 @@ from geopy.geocoders import Nominatim
 import pandas as pd
 import numpy as np
 
+
+
 '''
 # Welcome to TaxiFare
 '''
 
-st.title("please enter the parameters of your ride :) ")
 
 pick_up_address = st.text_input('please enter your pick up location','')
-st.write('your current location is', pick_up_address)
 
 
 drop_off_address = st.text_input('please enter your drop off location','')
-st.write('you want to go to', drop_off_address)
-
 
 
 geolocator = Nominatim(user_agent="myApp")
-df = pd.DataFrame({'Location':
-             [pick_up_address, drop_off_address]})
 
-df[['latitude', 'longitude']] =  df['Location'].apply(
-    geolocator.geocode).apply(lambda x: pd.Series(
-        [x.latitude, x.longitude], index=['location_lat', 'location_long']))
+def geocode_address(address):
+    """Convert address string to (lat, lon, full_address) or None."""
+    if not address.strip():
+        return None
+    else:
+        result = geolocator.geocode(address)
+        if result:
+            return result.latitude, result.longitude, result.address
+        return None
 
-df
-
-def get_map_data():
-
-    return df[['latitude', 'longitude']]
-
-df = get_map_data()
-st.map(df)
-
+pick_up = geocode_address(pick_up_address)
+drop_off = geocode_address(drop_off_address)
 
 
 # Date and time
@@ -48,17 +43,6 @@ ride_datetime = st.datetime_input(
 # Passenger count
 passenger_count = st.number_input("Passenger Count", min_value=1, max_value=8, value=1, step=1)
 
-st.write("Ride details:", {
-    "datetime": ride_datetime,
-    "pickup_longitude": df['longitude'][0],
-    "pickup_latitude": df['latitude'][0],
-    "dropoff_longitude": df['longitude'][1],
-    "dropoff_latitude": df['latitude'][1],
-    "passenger_count": passenger_count
-})
-'''
-## Please press the below button to get the predicted price of your ride
-'''''
 
 
 url = 'https://taxifare.lewagon.ai/predict'
@@ -66,12 +50,26 @@ url = 'https://taxifare.lewagon.ai/predict'
 if st.button("Get Fare Prediction"):
     params = {
         "pickup_datetime": str(ride_datetime),
-        "pickup_longitude": df['longitude'][0],
-        "pickup_latitude": df['latitude'][0],
-        "dropoff_longitude": df['longitude'][1],
-        "dropoff_latitude": df['latitude'][1],
+        "pickup_longitude": pick_up[1],
+        "pickup_latitude": pick_up[0],
+        "dropoff_longitude": drop_off[1],
+        "dropoff_latitude": drop_off[0],
         "passenger_count": passenger_count
     }
     response = requests.get(url, params=params)
     prediction = response.json()
-    st.write("Predicted fare:", prediction)
+    prediction = response.json()["fare"]
+    st.success(f"Predicted fare: ${prediction:.2f}")
+    st.balloons()
+
+map_data = []
+
+if pick_up is not None:
+    map_data.append({"latitude": pick_up[0], "longitude": pick_up[1]})
+
+if drop_off is not None:
+    map_data.append({"latitude": drop_off[0], "longitude": drop_off[1]})
+
+if map_data:
+    df = pd.DataFrame(map_data)
+    st.map(df)
